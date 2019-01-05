89c0450150d2d3a368cc867a844024486a01889d
Richard S. Leung <r4ds@lenyip.com>
2019-01-05 21:39:39 +0900

    Create Setting_up_new_user_for_website.sh

---
+++ Setting_up_new_user_for_website.sh
@@ -0,0 +1,12 @@
+# Setting PHP-FPM user to nginx
+
+username=neko9
+
+useradd $username
+usermod -a -G nginx $username
+id $username
+su -c "mkdir /home/$username/public_html" $username
+chgrp nginx /home/$username/public_html
+chmod g+rwxs /home/$username/public_html
+
+su -c 'cat /dev/zero | ssh-keygen -t ed25519 -q -N ""' $username
