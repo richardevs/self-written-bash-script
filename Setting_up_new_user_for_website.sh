e4df6c0047a0d56c369b492d83344feba5bee6b5
Richard S. Leung <r4ds@lenyip.com>
2019-01-05 21:42:47 +0900

    Update Setting_up_new_user_for_website.sh

--- Setting_up_new_user_for_website.sh
+++ Setting_up_new_user_for_website.sh
@@ -10,3 +10,5 @@ chgrp nginx /home/$username/public_html
 chmod g+rwxs /home/$username/public_html
 
 su -c 'cat /dev/zero | ssh-keygen -t ed25519 -q -N ""' $username
+su -c "cp /home/$username/.ssh/id_ed25519.pub /home/$username/.ssh/authorized_keys" $username
+chmod 600 /home/$username/.ssh/authorized_keys
