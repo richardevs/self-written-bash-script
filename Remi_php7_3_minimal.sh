59e9f9037445c9672a9517fc9b1fe5fc0d575cdf
Richard S. Leung <r4ds@lenyip.com>
2019-01-05 21:15:47 +0900

    Create Remi_php7_3_minimal.sh

---
+++ Remi_php7_3_minimal.sh
@@ -0,0 +1,10 @@
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
+rm -f epel-release-latest-7.noarch.rpm
+rm -f remi-release-7.rpm
+yum install yum-utils -y
+yum-config-manager --enable remi-php73
+yum install php php-fpm php-opcache -y
+
+systemctl enable php-fpm
