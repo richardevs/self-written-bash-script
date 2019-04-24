8dd458beba2c818f02433b28b7468546210ae901
Richard S. Leung <r4ds@lenyip.com>
2019-04-24 12:29:47 +0900

    Update Setting_up_new_user_for_website.sh

--- Setting_up_new_user_for_website.sh
+++ Setting_up_new_user_for_website.sh
@@ -8,6 +8,8 @@ id $username
 su -c "mkdir /home/$username/public_html" $username
 chgrp nginx /home/$username/public_html
 chmod g+rwxs /home/$username/public_html
+# May have to re-enforce after unzip or such
+# chmod -R g=rwx .
 
 su -c 'cat /dev/zero | ssh-keygen -t ed25519 -q -N ""' $username
 su -c "cp /home/$username/.ssh/id_ed25519.pub /home/$username/.ssh/authorized_keys" $username
